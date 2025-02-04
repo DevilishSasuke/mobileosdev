015eff9e8d22cab086d178deebed9aa7f6351bcf
DevilishSasuke <petrovich_uasya@mail.ru>
2025-02-05 02:09:44 +0300

    lab3 added downloading xml file

--- Lab3/curex/src/main/java/com/example/curex/MainActivity.kt
+++ Lab3/curex/src/main/java/com/example/curex/MainActivity.kt
@@ -1,20 +1,52 @@
 package com.example.curex
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.URL
+import java.net.HttpURLConnection
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val testText = findViewById<TextView>(R.id.testText)
+        val url = "https://www.cbr.ru/scripts/XML_daily.asp"
+
+        GlobalScope.launch(Dispatchers.IO) {
+            val xmlFileData = downloadXmlFile(url)
+
+            withContext(Dispatchers.Main) {
+                testText.text = xmlFileData
+            }
         }
     }
+
+    private fun downloadXmlFile(url: String): String? {
+        val connection = URL(url).openConnection() as HttpURLConnection
+
+        val response = try {
+            connection.requestMethod = "GET"
+            connection.connect()
+
+            if (connection.responseCode == HttpURLConnection.HTTP_OK) {
+                val reader = BufferedReader(InputStreamReader(connection.inputStream))
+                reader.use { it.readText() }
+            } else {
+                null
+            }
+        } finally {
+            connection.disconnect()
+        }
+
+        return response
+    }
+
+
 }
